1b910c403a74a54de2911e80887f610ddd922929
Scott Bowles <shbowles@gmail.com>
2020-07-20 19:23:15 -0400

    Add count controller param validation & handle errors & handle count not found

--- controllers/inventoryCountController.js
+++ controllers/inventoryCountController.js
@@ -51,44 +51,68 @@ exports.count_home = function countHome(req, res, next) {
   }
 };
 
-exports.count_detail_get = function countDetailGet(req, res, next) {
-  InventoryCount.findById(req.params.id)
-    .populate({
-      path: "countedQuantities.item",
-      populate: "category",
-    })
-    .exec((err, count) => {
-      if (err) {
-        return next(err);
-      }
-      count.countedQuantities.sort((a, b) => {
-        if (a.item.sku !== b.item.sku) {
-          return a.item.sku > b.item.sku ? 1 : -1;
+exports.count_detail_get = [
+  param("id").isMongoId().withMessage("Invalid count id").escape(),
+  function countDetailGet(req, res, next) {
+    const { errors } = validationResult(req);
+    if (errors.length > 0) {
+      return res.render("countDetail", { title: "Count Detail", errors });
+    }
+    InventoryCount.findById(req.params.id)
+      .populate({
+        path: "countedQuantities.item",
+        populate: "category",
+      })
+      .exec((err, count) => {
+        if (err) {
+          return next(err);
         }
-        return a.item.name > b.item.name ? 1 : -1;
+        if (count === null) {
+          return res.render("countDetail", {
+            title: "Count Detail",
+            errors: [{ msg: "Count not found" }],
+          });
+        }
+        count.countedQuantities.sort((a, b) => {
+          if (a.item.sku !== b.item.sku) {
+            return a.item.sku > b.item.sku ? 1 : -1;
+          }
+          return a.item.name > b.item.name ? 1 : -1;
+        });
+        return res.render("countDetail", { title: "Count Detail", count });
       });
-      res.render("countDetail", { title: "Count Detail", count });
-    });
-};
+  },
+];
 
-exports.count_detail_post = async function countDetailPost(req, res, next) {
-  const count = await InventoryCount.findById(req.params.id)
-    .populate({ path: "countedQuantities.item", populate: "category" })
-    .exec();
-  console.log({ count });
-  if (count.submitted) {
-    res.render("countDetail", {
-      title: "Count Detail",
-      count,
-      error: "Cannot update a submitted count",
-    });
-    return;
-  }
-  count.dateSubmitted = Date.now();
-  await count.save().catch((err) => next(err));
+exports.count_detail_post = [
+  param("id").isMongoId().withMessage("Invalid count id").escape(),
+  async function countDetailPost(req, res, next) {
+    const { errors } = validationResult(req);
+    if (errors.length > 0) {
+      return res.render("countDetail", { title: "Count Detail", errors });
+    }
+    const count = await InventoryCount.findById(req.params.id)
+      .populate({ path: "countedQuantities.item", populate: "category" })
+      .exec();
+    if (count === null) {
+      return res.render("countDetail", {
+        title: "Count Detail",
+        errors: [{ msg: "Count not found" }],
+      });
+    }
+    if (count.submitted) {
+      return res.render("countDetail", {
+        title: "Count Detail",
+        count,
+        errors: [{ msg: "Cannot update a submitted count" }],
+      });
+    }
+    count.dateSubmitted = Date.now();
+    await count.save().catch((err) => next(err));
 
-  res.redirect(count.url);
-};
+    return res.redirect(count.url);
+  },
+];
 
 exports.count_create_get = async function countCreateGet(req, res, next) {
   const { filter } = req.query || undefined;
@@ -232,80 +256,105 @@ exports.count_create_post = [
   },
 ];
 
-exports.count_update_get = async function countUpdateGet(req, res, next) {
-  const count = await InventoryCount.findById(req.params.id)
-    .populate({
-      path: "countedQuantities.item",
-      populate: "category",
-    })
-    .exec();
+exports.count_update_get = [
+  param("id").isMongoId().withMessage("Invalid count id").escape(),
 
-  count.countedQuantities.sort((a, b) => {
-    if (a.item.sku !== b.item.sku) {
-      return a.item.sku > b.item.sku ? 1 : -1;
+  async function countUpdateGet(req, res, next) {
+    const { errors } = validationResult(req);
+    if (errors.length > 0) {
+      return res.render("countForm", { title: "Update Count", errors });
     }
-    return a.item.name > b.item.name ? 1 : -1;
-  });
 
-  // Only matters here whether "By Category" or not, but this gives consistency to the front end & for POST
-  const filter =
-    // eslint-disable-next-line no-nested-ternary
-    count.type === "Full"
-      ? "Full"
-      : count.type === "Ad Hoc"
-      ? "AdHoc"
-      : "By Category";
+    const count = await InventoryCount.findById(req.params.id)
+      .populate({
+        path: "countedQuantities.item",
+        populate: "category",
+      })
+      .exec();
 
-  if (count.submitted) {
-    res.render("countDetail", {
-      title: "Count Detail",
+    if (count === null) {
+      return res.render("countForm", {
+        title: "Update Count",
+        errors: [{ msg: "Count not found" }],
+      });
+    }
+
+    count.countedQuantities.sort((a, b) => {
+      if (a.item.sku !== b.item.sku) {
+        return a.item.sku > b.item.sku ? 1 : -1;
+      }
+      return a.item.name > b.item.name ? 1 : -1;
+    });
+
+    // Only matters here whether "By Category" or not, but this gives consistency to the front end & for POST
+    const filter =
+      // eslint-disable-next-line no-nested-ternary
+      count.type === "Full"
+        ? "Full"
+        : count.type === "Ad Hoc"
+        ? "AdHoc"
+        : "By Category";
+
+    if (count.submitted) {
+      return res.render("countDetail", {
+        title: "Count Detail",
+        count,
+        filter,
+        errors: [{ msg: "Cannot update a submitted count" }],
+      });
+    }
+
+    const items = await Item.find({})
+      .populate("category")
+      .exec()
+      .catch((err) => next(err));
+
+    // If category count, grab the category from the first item in the count and filter accordingly
+    // If that item has no category, filter for items with no category
+    const filteredItems =
+      filter === "Full" || filter === "AdHoc"
+        ? items
+        : items.filter((item) => {
+            if (!count.countedQuantities[0].item.category) {
+              return !item.category;
+            }
+            if (!item.category) {
+              return false;
+            }
+            return (
+              item.category.name ===
+              count.countedQuantities[0].item.category.name
+            );
+          });
+
+    const countHash = {};
+
+    count.countedQuantities.forEach((countItem) => {
+      const id = countItem.item._id.toString();
+      countHash[id] = countItem.quantity;
+    });
+
+    const itemsModifiedByCount = [];
+
+    filteredItems.forEach((item) => {
+      const newItem = item;
+      newItem.quantityInStock =
+        countHash[newItem._id.toString()] || item.quantityInStock;
+      itemsModifiedByCount.push(newItem);
+    });
+
+    itemsModifiedByCount.sort((item1, item2) =>
+      item1.name.toLowerCase() > item2.name.toLowerCase() ? 1 : -1
+    );
+
+    return res.render("countForm", {
+      title: "Update Count",
+      items: itemsModifiedByCount,
       count,
       filter,
-      error: "Cannot update a submitted count",
     });
-  }
-
-  const items = await Item.find({})
-    .populate("category")
-    .exec()
-    .catch((err) => next(err));
-
-  // If category count, grab the category from the first item in the count and filter accordingly
-  const filteredItems =
-    filter === "Full" || filter === "AdHoc"
-      ? items
-      : items.filter(
-          (item) =>
-            item.category.name === count.countedQuantities[0].item.category.name
-        );
-
-  const countHash = {};
-
-  count.countedQuantities.forEach((countItem) => {
-    const id = countItem.item._id.toString();
-    countHash[id] = countItem.quantity;
-  });
-
-  const itemsModifiedByCount = [];
-
-  filteredItems.forEach((item) => {
-    const newItem = item;
-    newItem.quantityInStock =
-      countHash[newItem._id.toString()] || item.quantityInStock;
-    itemsModifiedByCount.push(newItem);
-  });
-
-  itemsModifiedByCount.sort((item1, item2) =>
-    item1.name.toLowerCase() > item2.name.toLowerCase() ? 1 : -1
-  );
-
-  res.render("countForm", {
-    title: "Update Count",
-    items: itemsModifiedByCount,
-    count,
-    filter,
-  });
-};
+  },
+];
 
 exports.count_update_post = [
   // remove empty items
@@ -319,11 +368,21 @@ exports.count_update_post = [
   body("items.*.quantity").isInt({ lt: 10000000 }).escape(),
   body("filter").escape(),
   body("submitButton").isIn(["submit", "save"]).escape(),
+  param("id").isMongoId().withMessage("Invalid count id").escape(),
 
   async function updatePost(req, res, next) {
     // grab errors & filter
     const { errors } = validationResult(req);
     const { filter } = req.body;
+
+    // if the countId from params is bad, handle before we try to fetch the count
+    const paramErrors = errors.filter((error) => error.location === "params");
+    if (paramErrors.length > 0) {
+      return res.render("countForm", {
+        title: "Update Count",
+        errors,
+      });
+    }
 
     // fetch count
     const count = await InventoryCount.findById(req.params.id)
@@ -333,10 +392,17 @@ exports.count_update_post = [
       })
       .exec();
 
+    if (count === null) {
+      return res.render("countForm", {
+        title: "Update Count",
+        errors: [{ msg: "Count not found" }],
+      });
+    }
+
     // if count has already been submitted, render with error message
     if (count.submitted) {
       if (count.submitted) {
-        res.render("countDetail", {
+        return res.render("countDetail", {
           title: "Count Detail",
           count,
           error: "Cannot update a submitted count",
@@ -346,7 +412,7 @@ exports.count_update_post = [
 
     // define category if a category count
     const category =
-      filter === "By Category"
+      filter === "By Category" && count.countedQuantities[0].item.category
         ? count.countedQuantities[0].item.category._id
         : undefined;
 
@@ -367,8 +433,8 @@ exports.count_update_post = [
       // console.log({ items });
 
       const filteredItems = category
-        ? items
-        : items.filter((item) => item.category._id === category);
+        ? items.filter((item) => item.category._id === category)
+        : items;
 
       const countHash = {};
       count.countedQuantities.forEach((countItem) => {
@@ -388,75 +454,115 @@ exports.count_update_post = [
         item1.name.toLowerCase() > item2.name.toLowerCase() ? 1 : -1
       );
 
-      res.render("countForm", {
+      return res.render("countForm", {
         title: "Update Count",
         items: itemsModifiedByCount,
         count,
         filter,
         errors,
       });
-    } else {
-      // no errors: save count. if submitting, update all of the items in count. if saving, save count alone.
+    }
 
-      // eslint-disable-next-line no-lonely-if
-      if (req.body.submitButton === "submit") {
-        // save count, update each item whose quantity was changed
-        await Promise.all([
-          count.countedQuantities.map(async (qty) => {
-            const item = await Item.findById(qty.item).exec();
-            if (item.quantityInStock !== qty.quantity) {
-              item.quantityInStock = qty.quantity;
-              item.qtyLastUpdated = Date.now();
-              return item.save();
-            }
-            return item;
-          }),
-        ])
-          .then(async () => {
-            count.dateSubmitted = Date.now();
-            const savedCount = await count.save();
-            res.redirect(savedCount.url);
-          })
-          .catch((err) => next(err));
-      } else {
-        // req.body.submit === "save" -- save count only
-        count
-          .save()
-          .catch((err) => next(err))
-          .then((savedCount) => res.redirect(savedCount.url));
+    // no errors: save count. if submitting, update all of the items in count. if saving, save count alone.
+    if (req.body.submitButton === "submit") {
+      // save count, update each item whose quantity was changed
+      return Promise.all([
+        count.countedQuantities.map(async (qty) => {
+          const item = await Item.findById(qty.item).exec();
+          if (item.quantityInStock !== qty.quantity) {
+            item.quantityInStock = qty.quantity;
+            item.qtyLastUpdated = Date.now();
+            return item.save();
+          }
+          return item;
+        }),
+      ])
+        .then(async () => {
+          count.dateSubmitted = Date.now();
+          const savedCount = await count.save();
+          res.redirect(savedCount.url);
+        })
+        .catch((err) => next(err));
+    }
+
+    // req.body.submit === "save" -- save count only
+    return count
+      .save()
+      .catch((err) => next(err))
+      .then((savedCount) => res.redirect(savedCount.url));
+  },
+];
+
+exports.count_delete_get = [
+  param("id").isMongoId().withMessage("Invalid count id").escape(),
+
+  async function countDeleteGet(req, res, next) {
+    const { errors } = validationResult(req);
+    if (errors.length > 0) {
+      return res.render("countDelete", {
+        title: "Remove Inventory Count",
+        errors,
+      });
+    }
+    try {
+      const { id } = req.params;
+      const count = await InventoryCount.findById(id)
+        .populate({
+          path: "countedQuantities.item",
+          populate: "category",
+        })
+        .exec();
+
+      if (count === null) {
+        return res.render("countDelete", {
+          title: "Remove Inventory Count",
+          errors: [{ msg: "Count not found" }],
+        });
       }
+
+      count.countedQuantities.sort((a, b) => {
+        if (a.item.sku !== b.item.sku) {
+          return a.item.sku > b.item.sku ? 1 : -1;
+        }
+        return a.item.name > b.item.name ? 1 : -1;
+      });
+
+      return res.render("countDelete", {
+        title: "Remove Inventory Count",
+        count,
+      });
+    } catch (error) {
+      return next(error);
     }
   },
 ];
 
-exports.count_delete_get = async function countDeleteGet(req, res, next) {
-  const { id } = req.params;
-  try {
-    const count = await InventoryCount.findById(id)
-      .populate({
-        path: "countedQuantities.item",
-        populate: "category",
-      })
-      .exec();
+exports.count_delete_post = [
+  param("id").isMongoId().withMessage("Invalid count id").escape(),
 
-    count.countedQuantities.sort((a, b) => {
-      if (a.item.sku !== b.item.sku) {
-        return a.item.sku > b.item.sku ? 1 : -1;
+  async function countDeletePost(req, res, next) {
+    const { errors } = validationResult(req);
+    if (errors.length > 0) {
+      return res.render("countDelete", {
+        title: "Remove Inventory Count",
+        errors,
+      });
+    }
+    try {
+      const count = await InventoryCount.findByIdAndRemove(
+        req.params.id
+      ).exec();
+
+      if (count === null) {
+        res.render("countDelete", {
+          title: "Remove Inventory Count",
+          errors: [{ msg: "Count not found" }],
+        });
       }
-      return a.item.name > b.item.name ? 1 : -1;
-    });
 
-    res.render("countDelete", { title: "Remove Inventory Count", count });
-  } catch (error) {
-    return next(error);
-  }
-};
-
-exports.count_delete_post = async function countDeletePost(req, res, next) {
-  try {
-    await InventoryCount.findByIdAndRemove(req.params.id).exec();
-    res.redirect("/inventory/counts");
-  } catch (error) {
-    return next(error);
-  }
-};
+      return res.redirect("/inventory/counts");
+    } catch (error) {
+      return next(error);
+    }
+  },
+];
