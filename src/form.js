594ba08a2bcc213be88119d3fd931a2b7680a427
Scott Bowles <shbowles@gmail.com>
2020-07-29 22:35:53 -0400

    Bring focus to password fields when they appear

--- src/form.js
+++ src/form.js
@@ -1,17 +1,20 @@
 const formTop = document.querySelector(".form-top");
 const passwordGroupTop = document.querySelector(".password-group-top");
 const formGroupsTop = [...document.querySelectorAll(".form-group-top")];
+const passwordInputTop = document.querySelector(".password-input-top");
 
 formTop.addEventListener("submit", (e) => {
   if ([...passwordGroupTop.classList].includes("hidden")) {
     e.preventDefault();
     formGroupsTop.forEach((formGroup) => formGroup.classList.toggle("hidden"));
+    passwordInputTop.focus();
   }
 });
 
 const formBotton = document.querySelector(".form-bottom");
 const passwordGroupBotton = document.querySelector(".password-group-bottom");
 const formGroupsBotton = [...document.querySelectorAll(".form-group-bottom")];
+const passwordInputBottom = document.querySelector(".password-input-bottom");
 
 formBotton.addEventListener("submit", (e) => {
   if ([...passwordGroupBotton.classList].includes("hidden")) {
@@ -19,5 +22,6 @@ formBotton.addEventListener("submit", (e) => {
     formGroupsBotton.forEach((formGroup) =>
       formGroup.classList.toggle("hidden")
     );
+    passwordInputBottom.focus();
   }
 });
