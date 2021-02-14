319412320b4076b0907c18022abf127be73a2128
scottBowles <shbowles@gmail.com>
2021-02-14 01:16:55 -0500

    Add utils for sorting countedQuantities

--- controllers/utils.js
+++ controllers/utils.js
@@ -1,24 +1,21 @@
-const multer = require("multer");
-const Order = require("../models/order");
+const multer = require('multer');
+const Order = require('../models/order');
 
 const uploadImage = multer({
-  dest: "public/images/",
+  dest: 'public/images/',
   fileFilter: (req, file, cb) => {
     if (!file.mimetype.match(/jpg$|png$|jpeg/)) {
-      cb(new Error("Filetype must be .png, .jpg or .jpeg"), false);
+      cb(new Error('Filetype must be .png, .jpg or .jpeg'), false);
     } else {
       cb(null, true);
     }
   },
   limits: { fieldSize: 1000000 },
-}).single("imageUpload");
+}).single('imageUpload');
 
 // create a hash of items -- { id: totalQuantityOnOrder }
 async function getQuantitiesOnOrder() {
-  const orders = await Order.find(
-    { status: "Ordered" },
-    "orderedItems"
-  ).exec();
+  const orders = await Order.find({ status: 'Ordered' }, 'orderedItems').exec();
 
   const itemQtyHash = {};
   orders.forEach((order) => {
@@ -32,6 +29,27 @@ async function getQuantitiesOnOrder() {
   return itemQtyHash;
 }
 
-module.exports = {
-  uploadImage, getQuantitiesOnOrder,
+function sortCountedQuantitiesByCategoryThenName(item1, item2) {
+  const cat1 = item1.category ? item1.category.name : '(None)';
+  const cat2 = item2.category ? item2.category.name : '(None)';
+  if (cat1 !== cat2) {
+    if (cat1 === '(None)') return 1;
+    if (cat2 === '(None)') return -1;
+    return cat1.toLowerCase() > cat2.toLowerCase() ? 1 : -1;
+  }
+  return item1.name.toLowerCase() > item2.name.toLowerCase() ? 1 : -1;
 }
+
+function sortCountedQuantitiesBySkuThenName(a, b) {
+  if (a.item.sku !== b.item.sku) {
+    return a.item.sku > b.item.sku ? 1 : -1;
+  }
+  return a.item.nameNormalized > b.item.nameNormalized ? 1 : -1;
+}
+
+module.exports = {
+  uploadImage,
+  getQuantitiesOnOrder,
+  sortCountedQuantitiesByCategoryThenName,
+  sortCountedQuantitiesBySkuThenName,
+};
