4bbea08dbef790d231d69276a1d9e5a35ef09ada
scottBowles <shbowles@gmail.com>
2021-02-14 01:15:21 -0500

    Add nameNormalized virtual

--- models/item.js
+++ models/item.js
@@ -1,13 +1,13 @@
-const moment = require("moment");
-const mongoose = require("mongoose");
+const moment = require('moment');
+const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
 const ItemSchema = new Schema({
   name: { type: String, required: true, maxlength: 40 },
   description: { type: String, maxlength: 256 },
-  category: { type: Schema.Types.ObjectId, ref: "Category" },
-  sku: { type: String, default: "Not marked for sale", maxlength: 24 },
+  category: { type: Schema.Types.ObjectId, ref: 'Category' },
+  sku: { type: String, default: 'Not marked for sale', maxlength: 24 },
   price: { type: Number, max: 9999999 },
   quantityInStock: { type: Number, required: true, max: 9999999 },
   qtyLastUpdated: { type: Date, required: true, default: Date.now() },
@@ -19,32 +19,36 @@ const ItemSchema = new Schema({
   },
 });
 
-ItemSchema.virtual("qtyLastUpdatedFormatted").get(
+ItemSchema.virtual('nameNormalized').get(function nameNormalized() {
+  return this.name.toUpperCase();
+});
+
+ItemSchema.virtual('qtyLastUpdatedFormatted').get(
   function lastQtyUpdateFormatted() {
-    return moment(this.qtyLastUpdated).format("MMMM Do YYYY, h:mm a");
+    return moment(this.qtyLastUpdated).format('MMMM Do YYYY, h:mm a');
   }
 );
 
-ItemSchema.virtual("qtyLastUpdatedBrief").get(function () {
-  return moment(this.orderDate).format("MMMM Do");
+ItemSchema.virtual('qtyLastUpdatedBrief').get(function () {
+  return moment(this.orderDate).format('MMMM Do');
 });
 
-ItemSchema.virtual("itemLastUpdatedFormatted").get(
+ItemSchema.virtual('itemLastUpdatedFormatted').get(
   function lastItemUpdateFormatted() {
-    return moment(this.itemLastUpdated).format("MMMM Do YYYY, h:mm a");
+    return moment(this.itemLastUpdated).format('MMMM Do YYYY, h:mm a');
   }
 );
 
-ItemSchema.virtual("itemLastUpdatedBrief").get(function () {
-  return moment(this.orderDate).format("MMMM Do");
+ItemSchema.virtual('itemLastUpdatedBrief').get(function () {
+  return moment(this.orderDate).format('MMMM Do');
 });
 
-ItemSchema.virtual("url").get(function itemUrl() {
+ItemSchema.virtual('url').get(function itemUrl() {
   return `/inventory/item/${this._id}`;
 });
 
-ItemSchema.virtual("forSale").get(function itemSku() {
-  return this.sku !== "Not marked for sale";
+ItemSchema.virtual('forSale').get(function itemSku() {
+  return this.sku !== 'Not marked for sale';
 });
 
-module.exports = mongoose.model("Item", ItemSchema);
+module.exports = mongoose.model('Item', ItemSchema);
